ef36d21ea5c173ec0c3be82282dd59ea856ada4d
alexikicommit <alexis.jeanjean@ynov.com>
2025-03-28 15:47:18 +0100

    feat(chats): front integration

---
+++ front/src/app/component/file-uploader/file-uploader.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { faCommentDots, faCommentSlash } from '@fortawesome/free-solid-svg-icons'
+
+@Component({
+  selector: 'app-file-uploader',
+  imports: [],
+  templateUrl: './file-uploader.component.html',
+  styleUrl: './file-uploader.component.scss'
+})
+export class FileUploaderComponent {
+
+}
--- front/src/app/component/home/home.component.ts
+++ front/src/app/component/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ChatBotComponent } from '../chat-bot/chat-bot.component'
+import { FileUploaderComponent } from '../file-uploader/file-uploader.component'
 
 @Component({
   selector: 'app-home',
-	imports: [
-		ChatBotComponent,
-	],
+  imports: [
+    ChatBotComponent,
+    FileUploaderComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
